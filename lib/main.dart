a8e3b27278c8d83e14aabd67e41bc52b4a4105f1
Mustafa Adel Zakaria <tifaadel3@gmail.com>
2023-08-02 04:33:40 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:basketball_counter_app/business_logic/cubit/cubit/counter_cubit.dart';
+import 'package:basketball_counter_app/counter_cubit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
